bf77bce45d11e97f961c67e081fd993cf5734d67
HP <khoinguyen17062002@gmail.com>
2018-08-07 22:58:05 +0700

    bla

---
+++ maps/map_genderator.py
@@ -0,0 +1,19 @@
+import game_object
+import pygame
+from addict import Dict
+import json
+def genarate_map(json_file_url):
+    text = open(json_file_url,'r').read()
+
+    map_dict = json.loads(text)
+    map = Dict(map_dict)
+    data = map['layer'][0]['data']
+    width = map_dict['width']
+    height = map_dict['height']
+
+
+
+
+
+if __name__== '__main__':
+    genarate_map('‪image/maps')
